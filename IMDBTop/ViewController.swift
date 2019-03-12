4a3b934fe761004a77e4f350b5e1431d54c00bf9
Камилла Б <kam.bad@yandex.ru>
2019-03-12 16:21:46 +0300

    Initial Commit
    Add files

---
+++ IMDBTop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMDBTop
+//
+//  Created by Камилла Бадаева on 12/03/2019.
+//  Copyright © 2019 Nikita Moiseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
